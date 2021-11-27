46bb5865133a72873f8c107611c6f1b540adb5f5
Stephen Haberman <stephen.haberman@gmail.com>
2021-11-27 17:43:19 -0600

    Add a test with a factory.

--- src/entities/Author.test.ts
+++ src/entities/Author.test.ts
@@ -1,10 +1,16 @@
-import { Author, EntityManager } from "src/entities";
+import { Author, EntityManager, newAuthor } from "src/entities";
 import { newEntityManager } from "src/setupTests";
 
 describe("Author", () => {
   it("can be created", async () => {
     const em = newEntityManager();
     const a = new Author(em, { firstName: "a1" });
+    await em.flush();
+  });
+
+  it("can be created with a factory", async () => {
+    const em = newEntityManager();
+    const a = newAuthor(em);
     await em.flush();
   });
 });
