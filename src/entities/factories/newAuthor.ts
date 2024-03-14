ebb8087386ca2d084398feb6d3968e1502441107
Stephen Haberman <stephen.haberman@gmail.com>
2024-03-13 23:32:51 -0500

    Bump joist.

--- src/entities/factories/newAuthor.ts
+++ src/entities/factories/newAuthor.ts
@@ -1,5 +1,5 @@
 import { DeepNew, FactoryOpts, New, newTestInstance } from "joist-orm";
-import { Author, EntityManager } from "./entities";
+import { Author, EntityManager } from "../entities";
 
 export function newAuthor(em: EntityManager, opts: FactoryOpts<Author> = {}): DeepNew<Author> {
   return newTestInstance(em, Author, opts);
--- src/entities/factories/newBook.ts
+++ src/entities/factories/newBook.ts
@@ -1,5 +1,5 @@
 import { DeepNew, FactoryOpts, New, newTestInstance } from "joist-orm";
-import { Book, EntityManager } from "./entities";
+import { Book, EntityManager } from "../entities";
 
 export function newBook(em: EntityManager, opts: FactoryOpts<Book> = {}): DeepNew<Book> {
   return newTestInstance(em, Book, opts);
